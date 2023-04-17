6bb284ab8f29237c87e9fe9e313e6c6031854900
danish0501 <alidanish373@gmail.com>
2023-04-17 17:35:43 +0530

    Done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -18,7 +18,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 // Do not change code below this line
